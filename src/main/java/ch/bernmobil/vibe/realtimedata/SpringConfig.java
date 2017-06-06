833688d22a87a10b6031e7f7e2aff5937e339582
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2017-06-06 13:51:09 +0200

    JOOQ configuration

--- src/main/java/ch/bernmobil/vibe/realtimedata/SpringConfig.java
+++ src/main/java/ch/bernmobil/vibe/realtimedata/SpringConfig.java
@@ -5,6 +5,9 @@ import javax.sql.DataSource;
 import ch.bernmobil.vibe.shared.UpdateHistoryRepository;
 import ch.bernmobil.vibe.shared.UpdateManager;
 import ch.bernmobil.vibe.shared.UpdateManagerRepository;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -60,19 +63,36 @@ public class SpringConfig {
         return new UpdateManager(mapperRepository, staticRepository, updateHistoryRepository, historySize, timeout);
     }
 
+    @Bean(name = "StaticDslContext")
+    public DSLContext staticDslContext(@Qualifier("StaticDataSource") DataSource dataSource) {
+        return getDslContext(dataSource);
+    }
+
+
+    @Bean(name = "MapperDslContext")
+    public DSLContext mapperDslContext(@Qualifier("MapperDataSource") DataSource dataSource) {
+        return getDslContext(dataSource);
+    }
+
+    private DSLContext getDslContext(DataSource dataSource) {
+        String dialectString = environment.getProperty("spring.jooq.sql-dialect").toUpperCase();
+        SQLDialect dialect = SQLDialect.valueOf(dialectString);
+        return DSL.using(dataSource, dialect);
+    }
+
     @Bean(name = "MapperRepository")
-    public UpdateManagerRepository mapperRepository(@Qualifier("MapperDataSource") DataSource mapperDataSource) {
-        return new UpdateManagerRepository(new JdbcTemplate(mapperDataSource));
+    public UpdateManagerRepository mapperRepository(@Qualifier("MapperDslContext") DSLContext mapperDslContext) {
+        return new UpdateManagerRepository(mapperDslContext);
     }
 
     @Bean(name = "StaticRepository")
-    public UpdateManagerRepository staticRepository(@Qualifier("StaticDataSource") DataSource staticDataSource) {
-        return new UpdateManagerRepository(new JdbcTemplate(staticDataSource));
+    public UpdateManagerRepository staticRepository(@Qualifier("StaticDslContext") DSLContext staticDslContext) {
+        return new UpdateManagerRepository(staticDslContext);
     }
 
     @Bean
-    public UpdateHistoryRepository updateHistoryRepository(@Qualifier("StaticDataSource")DataSource dataSource) {
-        return new UpdateHistoryRepository(dataSource);
+    public UpdateHistoryRepository updateHistoryRepository(@Qualifier("StaticDslContext")DSLContext staticDslContext) {
+        return new UpdateHistoryRepository(staticDslContext);
     }
 
     private DataSource createDataSource(String driver, String url, String username, String password) {
