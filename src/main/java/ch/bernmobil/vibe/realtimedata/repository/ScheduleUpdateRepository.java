544494d14d8f8d3a59ed21dcc772c81556f46e3c
matteopatisso <matteo.patisso@gmail.com>
2017-06-07 20:16:36 +0200

    use jooq batch instead of multiple single jooq inserts

--- src/main/java/ch/bernmobil/vibe/realtimedata/repository/ScheduleUpdateRepository.java
+++ src/main/java/ch/bernmobil/vibe/realtimedata/repository/ScheduleUpdateRepository.java
@@ -4,12 +4,14 @@ import static java.util.stream.Collectors.toList;
 import static org.jooq.impl.DSL.table;
 
 import ch.bernmobil.vibe.shared.contract.ScheduleUpdateContract;
-import ch.bernmobil.vibe.realtimedata.entity.ScheduleUpdate;
+import ch.bernmobil.vibe.shared.entitiy.ScheduleUpdate;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.UUID;
 import org.jooq.DSLContext;
 import org.jooq.Field;
+import org.jooq.InsertValuesStepN;
+import org.jooq.Record;
 import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -24,14 +26,14 @@ public class ScheduleUpdateRepository {
 
     public void save(Collection<ScheduleUpdate> scheduleUpdates) {
         Collection<Field<?>> fields = Arrays.stream(ScheduleUpdateContract.COLUMNS).map(DSL::field).collect(toList());
-        for(ScheduleUpdate scheduleUpdate : scheduleUpdates) {
-            dslContext
-                .insertInto(table(ScheduleUpdateContract.TABLE_NAME), fields)
-                .values(DSL.val(UUID.randomUUID()), DSL.val(scheduleUpdate.getSchedule()),
-                    DSL.value(scheduleUpdate.getActualArrival()),
-                    DSL.value(scheduleUpdate.getActualDeparture()))
-                .execute();
-        }
+        Collection<InsertValuesStepN<Record>> insertStatements = scheduleUpdates
+            .stream()
+            .map((su) -> dslContext.insertInto(table(ScheduleUpdateContract.TABLE_NAME), fields)
+                .values(DSL.val(UUID.randomUUID()),DSL.val(su.getSchedule()),
+                    DSL.val(su.getActualArrival()), DSL.val(su.getActualDeparture())))
+            .collect(toList());
+
+        dslContext.batch(insertStatements).execute();
     }
 
     public void deleteAll() {
