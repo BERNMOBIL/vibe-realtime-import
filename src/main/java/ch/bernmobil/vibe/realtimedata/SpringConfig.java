58e752f8848665d9e165570cbcb72097853120d1
matteopatisso <matteo.patisso@gmail.com>
2017-06-10 15:44:17 +0200

    fix sonarqube code smells

--- src/main/java/ch/bernmobil/vibe/realtimedata/SpringConfig.java
+++ src/main/java/ch/bernmobil/vibe/realtimedata/SpringConfig.java
@@ -10,11 +10,9 @@ import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 
